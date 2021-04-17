56c3b55842e0ab8505de82126002a7659e5d3727
cemaltinay <cem.altinay@outlook.com>
2021-04-18 00:45:08 +0300

    Button ve Modal Componentler oluşturuldu.

--- Client/Program.cs
+++ Client/Program.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Blazored.Modal;
+using FoodOrder.Client.Utils;
 
 namespace FoodOrder.Client
 {
@@ -18,7 +20,9 @@ namespace FoodOrder.Client
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddScoped<ModalManager>();
 
+            builder.Services.AddBlazoredModal();
             await builder.Build().RunAsync();
         }
     }
---
+++ Client/Utils/ModalManager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading.Tasks;
+using Blazored.Modal;
+using Blazored.Modal.Services;
+using FoodOrder.Client.CustomComponents.ModalComponent;
+
+namespace FoodOrder.Client.Utils
+{
+    public class ModalManager
+    {
+
+        public IModalService _modalService { get; }
+        public ModalManager(IModalService modalService)
+        {
+            _modalService = modalService;
+        }
+
+    
+
+        public async Task ShowMessageAsync(string title, string message, int duration=0)
+        {
+            ModalParameters mParams = new ModalParameters();
+            mParams.Add("Message", message);
+           var modalRef= _modalService.Show<ShowMessagePupupComponent>(title, mParams);
+
+            if (duration > 0)
+            {
+                await Task.Delay(duration);
+                modalRef.Close();
+            }
+            //await modalRef.Result;
+        }
+
+        public async Task<bool> ConfirmationPopup(string title, string message)
+        {
+          ModalParameters mParams = new ModalParameters();
+            mParams.Add("Message", message);
+
+            var modalRef = _modalService.Show<ConfirmationPopupComponent>(title, mParams);
+           var result = await modalRef.Result;
+
+            return !result.Cancelled;
+        }
+            
+
+    }
+}
